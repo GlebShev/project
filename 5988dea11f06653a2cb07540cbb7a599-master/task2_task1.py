dfa52d82210d32ff3ae8f9a57abad803515d25f6
GlebShev <77424538+GlebShev@users.noreply.github.com>
2021-01-23 21:29:24 +0300

    Delete 5988dea11f06653a2cb07540cbb7a599-master directory

--- 5988dea11f06653a2cb07540cbb7a599-master/task2_task1.py
+++
@@ -1,3 +0,0 @@
-my_list = [True, None, 'hi!', 12, 12.4 ]
-for i in range(len(my_list)):
-    print(type(my_list[i]))
--- 5988dea11f06653a2cb07540cbb7a599-master/task2_task5.py
+++
@@ -1,6 +0,0 @@
-my_list = [7, 6, 5, 5, 3]
-num = int(input("Введите число:"))
-my_list.append(num)
-my_list.sort(reverse=True)
-
-print(f"Рейтинг: {my_list}")
